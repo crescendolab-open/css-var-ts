6b3e01444f1d063574ea7d655afeb8d4cc51c0ab
VdustR <VdustR@gmail.com>
2025-10-08 16:04:47 +0000

    Deploying to gh-pages from @ crescendolab-open/css-var-ts@d37c603ef1075c03bd83f6fd399753355c800d02 🚀

---
+++ assets/01_staticCssVarKey.stories-DDEiy0zK.js
@@ -0,0 +1,27 @@
+import{j as g}from"./jsx-runtime-D_zvdyIk.js";import{a as d,s as y}from"./cssVarUtils-Bhc2bgE3.js";import"./iframe-DVnvt7sE.js";import"./preload-helper-PPVm8Dsz.js";const l=d({recordKeyToCssVarKey:u=>`--static-${y(u)}`}),r=l.define({navy:"#001F3F",blue:"#0074D9",aqua:"#7FDBFF",teal:"#39CCCC",olive:"#3D9970",green:"#2ECC40",lime:"#01FF70",yellow:"#FFDC00",orange:"#FF851B",red:"#FF4136",fuchsia:"#F012BE",purple:"#B10DC9",maroon:"#85144B",white:"#FFFFFF",silver:"#DDDDDD",gray:"#AAAAAA",black:"#111111"}),a=l.define({primary:r.getValue("navy"),secondary:r.getValue("gray"),success:r.getValue("green"),warning:r.getValue("orange"),error:r.getValue("red"),background:r.getValue("white"),foreground:r.getValue("black")}),v=["blue","aqua","teal","olive","lime","orange","fuchsia","purple","maroon"],f=["foreground","primary","secondary","success","warning","error"],F=({text:u,color:p,variant:m})=>g.jsx("div",{style:{...r.cssProps,...a.cssProps,fontFamily:"system-ui, sans-serif",[a.getKey("primary")]:r.getValue(p),color:a.getValue(m),backgroundColor:a.getValue("background")},children:u}),h={component:F,args:{text:"Hello, CSS Variable!",color:"navy",variant:"foreground"},argTypes:{text:{control:"text",description:"Text to display"},color:{control:{type:"inline-radio"},options:v,description:"Color of the text"},variant:{control:{type:"inline-radio"},options:f,description:"Status of the text"}}},e={},s={args:{color:"fuchsia",variant:"primary"}},o={args:{color:"blue",variant:"primary"}},n={args:{variant:"secondary"}},t={args:{variant:"success"}},c={args:{variant:"warning"}},i={args:{variant:"error"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: "fuchsia",
+    variant: "primary"
+  }
+}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: "blue",
+    variant: "primary"
+  }
+}`,...o.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary"
+  }
+}`,...n.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "success"
+  }
+}`,...t.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "warning"
+  }
+}`,...c.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "error"
+  }
+}`,...i.parameters?.docs?.source}}};const x=["playground","fuchsiaPrimary","bluePrimary","secondary","success","warning","error"];export{x as __namedExportsOrder,o as bluePrimary,h as default,i as error,s as fuchsiaPrimary,e as playground,n as secondary,t as success,c as warning};
---
+++ assets/index.stories-W0JoOueU.js
@@ -0,0 +1,27 @@
+import{j as g}from"./jsx-runtime-D_zvdyIk.js";import{c as u}from"./cssVarUtils-Bhc2bgE3.js";import"./iframe-DVnvt7sE.js";import"./preload-helper-PPVm8Dsz.js";const r=u.define({navy:"#001F3F",blue:"#0074D9",aqua:"#7FDBFF",teal:"#39CCCC",olive:"#3D9970",green:"#2ECC40",lime:"#01FF70",yellow:"#FFDC00",orange:"#FF851B",red:"#FF4136",fuchsia:"#F012BE",purple:"#B10DC9",maroon:"#85144B",white:"#FFFFFF",silver:"#DDDDDD",gray:"#AAAAAA",black:"#111111"}),a=u.define({primary:r.getValue("navy"),secondary:r.getValue("gray"),success:r.getValue("green"),warning:r.getValue("orange"),error:r.getValue("red"),background:r.getValue("white"),foreground:r.getValue("black")}),d=["blue","aqua","teal","olive","lime","orange","fuchsia","purple","maroon"],y=["foreground","primary","secondary","success","warning","error"],v=({text:l,color:p,variant:m})=>g.jsx("div",{style:{...r.cssProps,...a.cssProps,fontFamily:"system-ui, sans-serif",[a.getKey("primary")]:r.getValue(p),color:a.getValue(m),backgroundColor:a.getValue("background")},children:l}),b={component:v,args:{text:"Hello, CSS Variable!",color:"navy",variant:"foreground"},argTypes:{text:{control:"text",description:"Text to display"},color:{control:{type:"inline-radio"},options:d,description:"Color of the text"},variant:{control:{type:"inline-radio"},options:y,description:"Status of the text"}}},e={},s={args:{color:"fuchsia",variant:"primary"}},o={args:{color:"blue",variant:"primary"}},n={args:{variant:"secondary"}},t={args:{variant:"success"}},c={args:{variant:"warning"}},i={args:{variant:"error"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:"{}",...e.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: "fuchsia",
+    variant: "primary"
+  }
+}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    color: "blue",
+    variant: "primary"
+  }
+}`,...o.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "secondary"
+  }
+}`,...n.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "success"
+  }
+}`,...t.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "warning"
+  }
+}`,...c.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    variant: "error"
+  }
+}`,...i.parameters?.docs?.source}}};const C=["playground","fuchsiaPrimary","bluePrimary","secondary","success","warning","error"];export{C as __namedExportsOrder,o as bluePrimary,b as default,i as error,s as fuchsiaPrimary,e as playground,n as secondary,t as success,c as warning};
