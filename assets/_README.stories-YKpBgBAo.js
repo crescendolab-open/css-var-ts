a910c67be3b84534787290e312fb437e7608cb04
VdustR <VdustR@gmail.com>
2025-10-08 12:19:02 +0000

    Deploying to gh-pages from @ crescendolab-open/css-var-ts@8a0fd11a3d05d61c6fb9f3b169cdd979810f363e 🚀

---
+++ assets/_README.stories-YKpBgBAo.js
@@ -0,0 +1,12 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{c as s}from"./emotion-css.esm-DJjk4Gmd.js";import"./emotion-utils.browser.esm-YKnV_D65.js";const c={component:e.Fragment},o={article:s({fontFamily:"system-ui, sans-serif",color:"#333","& a":{color:"#3498db"}})},r={render:()=>e.jsxs("article",{className:o.article,children:[e.jsx("h1",{children:"@crescendolab/css-var-ts"}),e.jsxs("p",{children:["Please refer to the"," ",e.jsx("a",{href:"https://github.com/crescendolab-open/css-var-ts",target:"_blank",rel:"noopener noreferrer",children:"repository"})," ","for more information."]})]})};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  render: () => <article className={classNameRecord.article}>
+      <h1>@crescendolab/css-var-ts</h1>
+      <p>
+        Please refer to the{" "}
+        <a href="https://github.com/crescendolab-open/css-var-ts" target="_blank" rel="noopener noreferrer">
+          repository
+        </a>{" "}
+        for more information.
+      </p>
+    </article>
+}`,...r.parameters?.docs?.source}}};const l=["README"];export{r as README,l as __namedExportsOrder,c as default};
